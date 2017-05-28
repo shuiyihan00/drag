4e2cd2700d00df07b3d2da578eabed2218f1e1f7
shuiyihan00 <1542638499@qq.com>
2017-05-28 12:27:29 +0800

    change drag.js from master

--- drag.js
+++ drag.js
@@ -4,15 +4,10 @@
 	var disY=0;
 	obj.onmousedown=function(ev){
 		disX=ev.pageX-obj.offsetLeft;
-		disY=ev.pageY-obj.offsetLeft
+		disY=ev.pageY-obj.offsetLeft;
 		document.onmousemove=function(){
-<<<<<<< HEAD
 			obj.style.left=ev.pageX-disX+'px';
-			obj.style.top=ev.pageY-disY+'px';
-=======
-			obj.style.left=ev.clientX-disX+"px";
-			obj.style.right=ev.clientY-disY+"px";
->>>>>>> new1
+			obj.style.top=ev.pageY-disY+'px';	
 		}
 	}
 }
