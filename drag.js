6c98def01c6f5c2fa74f57938381f5e4e0e1e0db
shuiyihan00 <1542638499@qq.com>
2017-05-28 12:04:57 +0800

    change drag.js new1

--- drag.js
+++ drag.js
@@ -5,5 +5,9 @@
 	obj.onmousedown=function(ev){
 		disX=ev.pageX-obj.offsetLeft;
 		disY=ev.pageY-obj.offsetLeft
+		document.onmousemove=function(){
+			obj.style.left=ev.clientX-disX+"px";
+			obj.style.right=ev.clientY-disY+"px";
+		}
 	}
 }
